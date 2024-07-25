063f7ef38e3b7e79b90fb06ed04df84e1ea614cb
ClementMadiot <clementmadiot09@gmail.com>
2024-07-25 14:22:04 +0200

    add all components

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 // @mui import
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./navbar/Sidebar";
+import {Sidebar, Videos} from "../index";
+// import Videos from "./video/Videos";
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -16,6 +17,12 @@ const Feed = () => {
         <Typography className="hidden md:block mt-6 text-white" variant="body2">
           Copyright © 2024 Web Design
         </Typography>
+      </Box>
+      <Box p={2} className='h-[90vh] overflow-y-auto flex-1'>
+        <Typography variant="h4" fontWeight="bold" mb={2 }>
+        New  <span className="text-[#F31503]">videos</span>
+        </Typography>
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,23 @@
-import ChannelDetail from "./ChannelDetail";
-import Feed from "./Feed";
+import ChannelDetail from "./Channel/ChannelDetail";
+import ChannelCard from "./Channel/ChannelCard";
+import Feed from "./feed/Feed";
+import SearchFeed from "./feed/SearchFeed";
 import Navbar from "./navbar/Navbar";
 import Searchbar from "./navbar/Searchbar";
 import Sidebar from "./navbar/Sidebar";
-import SearchFeed from "./SearchFeed";
-import VideoDetail from "./VideoDetails";
+import Videos from "./video/Videos";
+import VideoDetail from "./video/VideoDetail";
+import VideoCard from "./video/VideoCard";
 
-export { ChannelDetail, Feed, Navbar, Searchbar, Sidebar, SearchFeed, VideoDetail };
+export {
+  ChannelDetail,
+  ChannelCard,
+  Feed,
+  SearchFeed,
+  Navbar,
+  Searchbar,
+  Sidebar,
+  Videos,
+  VideoDetail,
+  VideoCard,
+};
