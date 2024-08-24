cfd1fb041ebe1f0f3e0dda086dc859f7df927ebc
ClementMadiot <clementmadiot09@gmail.com>
2024-08-24 14:57:25 +0200

    fetch Video on click

--- src/components/video/VideoDetail.jsx
+++ src/components/video/VideoDetail.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { link, useParams } from 'react-router-dom';
-import ReactPlayer from 'react-player';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
 import { Box, Typography, Stack } from "@mui/material";
-import { CheckCircle } from '@mui/icons-material';
+import { CheckCircle } from "@mui/icons-material";
 
-// local import 
-import {Video} from '../index'
-import { fetchFromAPI } from '../../utils/fetchFromAPI';
+// local import
+import { Videos } from "../index";
+import { fetchFromAPI } from "../../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  
-  return (
-    <Box>
-      
-    </Box>
-  )
-}
+  const [videoDetail, setVideoDetail] = useState(null)
+  const { id } = useParams();
 
-export default VideoDetail
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistic&id=${id}`)
+    .then((data) => setVideoDetail(data.item[0]))
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        {/* main video  */}
+        <Box flex={1}>
+          {/* wrapp videos on the right side  */}
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
