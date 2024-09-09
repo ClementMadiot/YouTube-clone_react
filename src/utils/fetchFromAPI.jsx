dcfd38571b269ef134093284014485f3e30db038
ClementMadiot <clementmadiot09@gmail.com>
2024-09-09 11:03:49 +0200

    add logo + fix env

--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const REACT_APP_RAPID_API_KEY = 'a1d802f5demshf21186a985b4d35p1abd9ajsnba5213f28547'
-
 const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
@@ -10,7 +8,7 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "x-rapidapi-key": REACT_APP_RAPID_API_KEY,
+    "x-rapidapi-key": import.meta.env.VITE_SOME_KEY,
     "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
   },
 };
